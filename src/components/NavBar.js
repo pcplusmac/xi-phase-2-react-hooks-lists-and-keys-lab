eebc1fd0afadb6efa07f67f831b7d0589f684cbe
el-sam <samurai.h@yahoo.com>
2023-12-11 22:48:03 +0800

    done, but not pass, no idea for why

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,9 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const mappedLinks = links.map(link => {<a key={link} href={`#${link}`}> {link} </a>})
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{mappedLinks}</nav>;
 }
 
 export default NavBar;
