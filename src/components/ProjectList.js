d5f26a57805816c4d55e6ce017c9a4a0ecb5e832
pc-sam <samurai.h@yahoo.com>
2023-12-26 23:17:26 +0800

    new update to previous commit

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,7 +3,7 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   const mappedProj = projects.map(project => (
-  <ProjectItem key={project.id} name = {project.name} about = {project.about} technologies = {project.technologies}/>))
+    <ProjectItem key={project.id} name = {project.name} about = {project.about} technologies = {project.technologies}/>))
   return (
     <div id="projects">
       <h2>My Projects</h2>
